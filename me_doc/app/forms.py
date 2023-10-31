8df8a5ce0d08a68133159bb7187083a3c0faf055
Manpreet Gulia <mgulia@bu.edu>
2023-10-31 18:08:22 -0400

    running patient entity

---
+++ me_doc/app/forms.py
@@ -0,0 +1,6 @@
+from django import forms  
+from app.models import Patient  
+class PatientForm(forms.ModelForm):  
+    class Meta:  
+        model = Patient  
+        fields = "__all__"  
--- me_doc/me_doc/asgi.py
+++ me_doc/me_doc/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MEDOC project.
+ASGI config for me_doc project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MEDOC.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "me_doc.settings")
 
 application = get_asgi_application()
